79236157688350110dec32eeb53823b542971942
mitchellkok <mitchellkokme@gmail.com>
2022-10-28 10:35:19 +0800

    Added updated SD card code

---
+++ sdcard/ESP32_SD/src/main.cpp
@@ -0,0 +1,87 @@
+/*
+  SD card read/write
+
+  This example shows how to read and write data to and from an SD card file
+  The circuit:
+   SD card attached to SPI bus as follows:
+ ** MOSI - pin 11
+ ** MISO - pin 12
+ ** CLK - pin 13
+ ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
+
+  created   Nov 2010
+  by David A. Mellis
+  modified 9 Apr 2012
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+*/
+
+#include <SPI.h>
+#include <SD.h>
+
+File myFile;
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
+  Serial.print("Initializing SD card...");
+
+  while (!SD.begin()) {
+    Serial.println("initialization failed!");
+    delay(2000);
+  }
+  Serial.println("initialization done.");
+
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  myFile = SD.open("/test2.txt", FILE_WRITE);
+
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Writing to test2.txt...");
+    myFile.println("testing 1, 2, 3.");
+    
+    Serial.println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test2.txt");
+  }
+  if (myFile) {
+    Serial.print("Writing to test2.txt...");
+    myFile.println("testing 4, 5, 6.");
+    Serial.println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test2.txt");
+  }
+  // close the file:
+    myFile.close();
+
+
+  // re-open the file for reading:
+  myFile = SD.open("/test2.txt");
+  if (myFile) {
+    Serial.println("test2.txt:");
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test2.txt");
+  }
+}
+
+void loop() {
+  // nothing happens after setup
+}
