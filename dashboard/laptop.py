f9e661f6c00f6bec7e18d4b10efa601ac491a1d1
mitchellkok <mitchellkokme@gmail.com>
2022-11-13 10:23:09 +0800

    Send JSON data over flask turbo

--- dashboard/laptop.py
+++ dashboard/laptop.py
@@ -7,7 +7,7 @@ import threading
 from flask import Flask, render_template
 from turbo_flask import Turbo
 
-PRELOAD = False 
+PRELOAD = False # Not needed anymore; keep just in case
 # connect to internet and set preload to True if js fails offline
 # once loaded, set preload to False and reload
 
@@ -15,8 +15,10 @@ app = Flask(__name__)
 turbo = Turbo(app)
 
 ipaddress = "192.168.4.1"
+result_dict = {}
 
 def begin_poll():
+    global result_dict
     print("Starting...")
     url = "http://" + ipaddress + '/events'
     requests.get(url, stream=True)
@@ -24,7 +26,11 @@ def begin_poll():
     cnt = 0
     with app.app_context():
         for msg in messages:
-            print(msg, cnt)
+            try:
+                result_dict = json.loads(msg.data)
+                print(cnt, result_dict)
+            except:
+                print("Could not parse JSON:", msg.data)
             cnt += 1
             turbo.push(turbo.replace(render_template('loadavg.html'), 'load'))
 
@@ -37,21 +43,28 @@ def before_first_request():
 def hello():
     print("running")
     if not PRELOAD:
-        r = requests.get(url(ipaddress, "/getdata"))
-        result_dict = json.loads(r.text)
-        print(result_dict)
+        pass
+        # http_resquest_json(ipaddress)
     return render_template('index.html')
+
+@app.context_processor
+def inject_load():
+    try:
+        data = [result_dict["atm_master_temp"],result_dict["atm_master_pressure"],result_dict["atm_master_humidity"]]
+        print("INJECT", data)
+    except Exception as e:
+        data = ["--", "--", "--"]
+    
+    return {'load1': data[0], 'load5': data[1], 'load15': data[2]}
+
+def http_resquest_json(addr):
+    r = requests.get(url(addr, "/getdata"))
+    result_dict = json.loads(r.text)
+    print(result_dict)
+    return result_dict
 
 def url(address, tag=""):
     return "http://" + address + tag
 
-@app.context_processor
-def inject_load():
-    load = [int(random.random() * 100) / 100 for _ in range(3)]
-    print("INJECT", load)
-    return {'load1': load[0], 'load5': load[1], 'load15': load[2]}
-
-
-# print(r.text)[:20÷0]
 if __name__ == "__main__":
     app.run(debug=True, port=8090, threaded=True)
