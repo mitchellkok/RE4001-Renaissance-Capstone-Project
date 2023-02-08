20dcbafcde82c8324f59a0f77d56402fa0972ad7
mitchellkok <mitchellkokme@gmail.com>
2023-02-08 10:51:56 +0800

    Cleaned up backend code

--- dashboard2/laptop2.py
+++ dashboard2/laptop2.py
@@ -15,16 +15,13 @@ TESTING = True # Toggle for test mode
 test_keys = ['date_time', 'm_t', 'm_p', 'm_h', 's_t', 's_p', 's_h', 'g_so2', 'g_t', 't_t', 'e_so2',
                 'co2', 'rssi', 'gps_sat', 'gps_hdop', 'gps_lat', 'gps_lng', 'gps_alt', 'gps_deg',
                 'gps_mps', 'gps_val', 'imu_tmp', 'imu_acx', 'imu_acy', 'imu_acz', 'imu_gyx',
-                'imu_gyy', 'imu_gyz']
+                'imu_gyy', 'imu_gyz', "tx_rssi", "rx_rssi", "m_bv", "m_bp", "s_bv", "s_bp"]
 
 app = Flask(__name__)
 CORS(app,resources={r"/*":{"origins":"*"}})
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 esp32_address = "192.168.4.1"
-input_data = {}
-output_data = {}
-datetime = 0
 df = pd.DataFrame() # global dataframe to store data
 
 # Set path of csv file (csv file will be saved in the same location laptop2.py)
@@ -37,9 +34,10 @@ def handle_connect():
     print('Client connected')
 
 def testing():
-    global datetime, df
+    global df
+    datetime = 0
     print("TESTING MODE...")
-    parse_data()
+    output_data = parse_data()
     while True:
         datetime +=1
         for i in output_data.keys():
@@ -52,7 +50,7 @@ def testing():
         eventlet.sleep(5)
     
 def begin_poll():
-    global input_data, output_data
+    global df
     print("Starting...")
     url = "http://" + esp32_address + '/events'
     requests.get(url, stream=True)
@@ -63,7 +61,7 @@ def begin_poll():
             try:
                 input_data = json.loads(msg.data)
                 print(cnt, input_data)
-                parse_data()
+                output_data = parse_data(input_data)
                 # TODO: use socketio.emit to send data to frontend (can refer to testing()) --> Test with actual ESP32 now
                 socketio.emit('data', output_data)
 
@@ -74,10 +72,10 @@ def begin_poll():
                 print("Could not parse JSON:", msg.data)
             cnt += 1           
 
-def parse_data():
-    global datetime, input_data, output_data, data
+def parse_data(input_data={}):
     try:
-        datetime = (
+        output_data = input_data
+        output_data["date_time"] = (
             str(input_data["date_day"]).zfill(2) + "/" 
             + str(input_data["date_month"]).zfill(2) + "/" 
             + str(input_data["date_year"]).zfill(2) + " "
@@ -85,45 +83,10 @@ def parse_data():
             + str(input_data["date_min"]).zfill(2) + ":"
             + str(input_data["date_sec"]).zfill(2)
             )
-        output_data = {
-            'date_time': datetime,
-            'm_t':      input_data['m_t'], 
-            'm_p':      input_data["m_p"], 
-            'm_h':      input_data["m_h"],
-            's_t':      input_data["s_t"],
-            's_p':      input_data["s_p"], 
-            's_h':      input_data["s_h"],
-            'g_so2':    input_data["g_so2"],
-            'g_t':      input_data['g_t'],
-            't_t':      input_data["t_t"],
-            'e_so2':    input_data["e_so2"],
-            'co2':      input_data["co2"],
-            'tx_rssi':  input_data["tx_rssi"],
-            'rx_rssi':  input_data["rx_rssi"],
-            'gps_sat':  input_data["gps_sat"],
-            'gps_hdop': input_data["gps_hdop"],
-            'gps_lat':  input_data["gps_lat"],
-            'gps_lng':  input_data["gps_lng"],
-            'gps_alt':  input_data["gps_alt"],
-            'gps_deg':  input_data["gps_deg"],
-            'gps_mps':  input_data["gps_mps"],
-            'gps_val':  input_data["gps_val"],
-            'imu_tmp':  input_data["imu_tmp"],
-            'imu_acx':  input_data["imu_acx"],
-            'imu_acy':  input_data["imu_acy"],
-            'imu_acz':  input_data["imu_acz"],
-            'imu_gyx':  input_data["imu_gyx"],
-            'imu_gyy':  input_data["imu_gyy"],
-            'imu_gyz':  input_data["imu_gyz"],
-            'm_bv':     input_data["m_bv"],
-            'm_bp':     input_data["m_bp"],
-            's_bv':     input_data["s_bv"],
-            's_bp':     input_data["s_bp"],
-            }
     except Exception as e:
-        datetime = 0
         for i in test_keys:
             output_data[i] = "--"
+    return output_data
 
 def http_resquest_json(addr):
     r = requests.get("http://" + addr + "/getdata")
